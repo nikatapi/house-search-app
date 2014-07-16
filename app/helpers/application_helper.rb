cd8f80d0e0562ab02bb39b11c275dfd8df1456c5
nikatapi <nikatapi@gmail.com>
2014-07-16 21:14:32 +0300

    added modal for login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,21 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  def resource_name
+	:user
+  end
+
+  def resource
+	@resource ||= User.new
+  end
+
+  def resource_class
+  devise_mapping.to
+end
+
+def devise_mapping
+
+@devise_mapping ||= Devise.mappings[:user]
+
+end
 end
