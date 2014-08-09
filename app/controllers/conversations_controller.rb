849d54e2d5c419665e520ac7c3fdde00bebd42ca
nikatapi <nikatapi@gmail.com>
2014-08-09 15:29:21 +0300

    changed messaging layouts and improved functionality

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -15,7 +15,7 @@ class ConversationsController < ApplicationController
 
   def reply
 	current_user.reply_to_conversation(conversation, *message_params(:body, :subject))
-	redirect_to conversation
+	redirect_to :conversations
   end
 
 
